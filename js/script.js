e36fb24102449bf0fc2bd9ed699d3df2596549c4
chris <ce-dev@gmx.de>
2024-08-02 22:14:46 +0200

    Change to nicer styling but without functions

--- js/script.js
+++ js/script.js
@@ -1,13 +1,26 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const posts = [
-        {
-            title: "My First Blog Post",
-            link: "posts/first-post.html",
-            date: "July 22, 2024",
-            content: "Welcome to my blog! This is my first post. Stay tuned for more updates."
-        }
-        // Add more posts here
-    ];
+document.addEventListener("DOMContentLoaded", async function() {
+    const postsJsonFilePath = "./content/posts.json";
+    // const posts = require(postsJsonFilePath);
+    // const posts = [
+    //     {
+    //         title: "My First Blog Post",
+    //         link: "posts/first-post.html",
+    //         date: "July 22, 2024",
+    //         content: "Welcome to my blog! This is my first post. Stay tuned for more updates."
+    //     }
+    //     // Add more posts here
+    // ];
+
+    let posts = [];
+
+    await fetch(postsJsonFilePath)
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            posts = json;
+
+            loadPosts();
+        } );
 
     const postsContainer = document.getElementById('posts');
 
@@ -19,13 +32,29 @@ document.addEventListener("DOMContentLoaded", function() {
             postElement.innerHTML = `
                 <h2><a href="${post.link}">${post.title}</a></h2>
                 <p>${post.date}</p>
-                <p>${post.content.substring(0, 100)}...</p>
+                <p>${post.contentSnippet}</p>
             `;
             postsContainer.appendChild(postElement);
         });
     }
 
-    displayPosts(posts); // Display all posts initially
+    async function fetchPostContent(post) {
+        const response = await fetch(post.link);
+        const text = await response.text();
+        const html = new DOMParser().parseFromString(text, 'text/html');
+        const articleContent = html.querySelector('#article').innerText;
+        post.content = articleContent;
+        post.contentSnippet = articleContent.substring(0, 100) + '...';
+    }
+
+    async function loadPosts() {
+        for (let post of posts) {
+            await fetchPostContent(post);
+        }
+        displayPosts(posts); // Display all posts initially
+    }
+
+    
 
     const searchInput = document.getElementById('search-input');
     searchInput.addEventListener('input', function() {
